7c375204ee8cdb06403c38796bf5f09b0dcb1792
michihito sakuma <michihito.sakuma@e-seikatsu.co.jp>
2024-07-05 02:46:13 +0900

    [fix] bfs

--- algorithm/BFS_sample.py
+++ algorithm/BFS_sample.py
@@ -2,27 +2,19 @@
 開始のノードからのそれぞれのノードの距離を求める場合。
 存在を確認するだけの場合、distじゃなくてvisitにしてbooleanでも可
 
-n：グラフの頂点の数
-m：グラフの辺の数
-start：経路探索のスタート位置
-pos：現在の位置
-nex：posと連結している頂点
-dist：startから各頂点の距離
+g: 連結リスト
+n: グラフの頂点の数
+m: グラフの辺の数
+start: 経路探索のスタート位置
+pos: 現在の位置
+nex: posと連結している頂点
+dist: startから各頂点の距離
 """
 
 import queue
 
-n, m = map(int, input().split())
 
-# 隣接リストの作成
-g = [[] for _ in range(n + 1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    g[a].append(b)
-    g[b].append(a)
-
-
-def bfs(start):
+def bfs(g, n, start):
 
     # 初期化などなど
     dist = [-1] * (n + 1)
@@ -39,7 +31,3 @@ def bfs(start):
                 q.put(nex)
 
     return dist
-
-
-for i in bfs(1):
-    print(i)
