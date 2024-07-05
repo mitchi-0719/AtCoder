acc62bf11a9481a07a17b4887569067503e51720
michihito sakuma <michihito.sakuma@e-seikatsu.co.jp>
2024-07-06 00:22:37 +0900

    [add] abc339

---
+++ ABC/339/B.py
@@ -0,0 +1,24 @@
+h, w, n = map(int, input().split())
+g = [[False for _ in range(w)] for __ in range(h)]
+
+x = 0
+y = 0
+dire = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+dir_i = 0
+
+for i in range(n):
+    g[y][x] = not g[y][x]
+
+    if not g[y][x]:
+        dir_i -= 1
+        x += dire[dir_i % 4][0]
+        y += dire[dir_i % 4][1]
+    else:
+        dir_i += 1
+        x += dire[dir_i % 4][0]
+        y += dire[dir_i % 4][1]
+
+for i in range(h):
+    for j in range(w):
+        print("#" if g[i][j] else ".", end="")
+    print()
