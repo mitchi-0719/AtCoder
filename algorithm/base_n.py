9e8fe11a50ad0922b5762d5df75b55d6ba1f195d
michihito sakuma <michihito.sakuma@e-seikatsu.co.jp>
2024-07-08 00:51:44 +0900

    [fix] base_n

--- algorithm/base_n.py
+++ algorithm/base_n.py
@@ -1,3 +1,8 @@
+"""
+10進数をn進数に変換する関数
+"""
+
+
 def base_n(num_10, n):
     str_n = ""
     while num_10:
@@ -6,3 +11,16 @@ def base_n(num_10, n):
         str_n += str(num_10 % n)
         num_10 //= n
     return str_n[::-1]
+
+
+"""
+n進数を10進数に変換する関数。
+"""
+
+
+def base_10(num_n, n):
+    num_10 = 0
+    for s in str(num_n):
+        num_10 *= n
+        num_10 += int(s)
+    return num_10
