fae22a1be72b35b11dbf7570c1da0172adfdc10a
mitchi <pg.saku0719@gmail.com>
2024-11-15 20:01:25 +0900

    [add] abc319

--- ABC/319/D.py
+++ ABC/319/D.py
@@ -1,3 +1,11 @@
-n, m = map(int, input().split())
-l = list(map(int, input().split()))
+from itertools import accumulate
+import math
 
+
+n, m = map(int, input().split())
+
+l = list(map(int, input().split()))
+l = [li + 1 for li in l]
+l_acc = list(accumulate(l))
+length = sum(l) - m
+w = max(*l, math.ceil(length // m)) - 1
