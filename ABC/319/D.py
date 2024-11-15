37e159c3959b34b01dae39c446f6416666784ae4
mitchi <pg.saku0719@gmail.com>
2024-11-16 00:24:43 +0900

    [add] abc319_d

--- ABC/319/D.py
+++ ABC/319/D.py
@@ -1,11 +1,30 @@
-from itertools import accumulate
-import math
-
-
 n, m = map(int, input().split())
 
 l = list(map(int, input().split()))
 l = [li + 1 for li in l]
-l_acc = list(accumulate(l))
-length = sum(l) - m
-w = max(*l, math.ceil(length // m)) - 1
+ng = max(l)
+ok = sum(l)
+ans = 0
+w = 0
+
+while ng < ok:
+    mid = (ok + ng) // 2
+    if mid == w:
+        break
+
+    s = 0
+    ans = 0
+    for li in l:
+        if s + li <= mid:
+            s += li
+        else:
+            ans += 1
+            s = li
+
+    w = mid
+    if ans >= m:
+        ng = mid
+    else:
+        ok = mid
+
+print(ok - 1)
