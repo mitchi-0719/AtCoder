fafd41d6dbe07381a5589a9a42d03a3a2cf9b2fd
mitchi <pg.saku0719@gmail.com>
2025-04-13 01:49:27 +0900

    abc304-c

--- ABC/304/C.py
+++ ABC/304/C.py
@@ -1,36 +1,40 @@
-import math
+import sys
+
+sys.setrecursionlimit(10**8)
 
 n, d = map(int, input().split())
-x = []
-y = []
+xy = [list(map(int, input().split())) for _ in range(n)]
+contacts = [[] for _ in range(n)]
+visited = [False] * n
 
-for i in range(n):
-    X, Y = map(int, input().split())
-    x.append(X)
-    y.append(Y)
+
+def yes_no(b):
+    return "Yes" if b else "No"
+
 
 def is_inside(x1, y1, x2, y2):
-    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) <= d**2
 
-inf_true_idx = {0}
-inf_false_idx = {i for i in range(1, n)}
-cont = True
-add_item = set([])
 
-while cont:
-    cont = False
-    inf_true_idx = inf_true_idx.union(add_item)
-    add_item.clear()
-    for i in inf_true_idx:
+def dfs(g, i, visited):
+    if visited[i]:
+        return
 
-        inf_false_idx = inf_false_idx - inf_true_idx
-        for j in inf_false_idx:
-            if d >= is_inside(x[i], y[i], x[j], y[j]):
-                add_item.add(j)
-                cont = True
+    visited[i] = True
+    for v in g[i]:
+        dfs(g, v, visited)
+
+
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        if i == j:
+            continue
+
+        if is_inside(*xy[i], *xy[j]):
+            contacts[i].append(j)
+            contacts[j].append(i)
+
+dfs(contacts, 0, visited)
 
 for i in range(n):
-    if set([i]).issubset(set(inf_true_idx)):
-        print("Yes")
-    else:
-        print("No")
+    print(yes_no(visited[i]))
