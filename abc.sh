602b8b4c3d64cc9d9a57cb74597dbf64c54e3814
mitchi <pg.saku0719@gmail.com>
2025-04-13 02:35:20 +0900

    abc-templete

--- abc.sh
+++ abc.sh
@@ -6,12 +6,11 @@ else
     else
         mkdir ./ABC/$1
     fi
-    touch ./ABC/$1/A.py
-    touch ./ABC/$1/B.py
-    touch ./ABC/$1/C.py
-    touch ./ABC/$1/D.py
-    code ./ABC/$1/D.py
-    code ./ABC/$1/C.py
-    code ./ABC/$1/B.py
-    code ./ABC/$1/A.py
+
+    for p in A B C D E; do
+        if [ ! -f "./ABC/$1/$p.py" ]; then
+            cp ./template/ABC.py "./ABC/$1/$p.py"
+        fi
+        code "./ABC/$1/$p.py"
+    done
 fi
