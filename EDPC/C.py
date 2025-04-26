de8642de909b61b0ae2f6c11cb13969ccbc33fb4
mitchi <pg.saku0719@gmail.com>
2025-04-26 16:48:15 +0900

    dp-3

--- EDPC/C.py
+++ EDPC/C.py
@@ -1,16 +1,14 @@
 n = int(input())
+abc = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[-1 for _ in range(n)] for __ in range(3)]
+dp = [[0] * 3 for __ in range(n + 1)]
 
-x = [list(map(int, input().split())) for _ in range(n)]
 
-dp[0][0] = dp[1][0] = dp[2][0] = 0
-
-print(dp)
-for i in range(1, n):
+for i in range(n):
     for j in range(3):
         for k in range(3):
-            if j != k:
-                dp[i][j] = max(dp[i][j], dp[i - 1][k] + x[i][j])
-print(dp)
-print(max([dp[n - 1][i] for i in range(3)]))
+            if j == k:
+                continue
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][k] + abc[i][j])
+
+print(max(dp[-1]))
