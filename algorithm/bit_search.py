2d940b791d9bd8ad956958178697b22feb34b966
mitchi <pg.saku0719@gmail.com>
2025-06-28 20:13:14 +0900

    ビット全探索

---
+++ algorithm/bit_search.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+
+def BitSearch(x, l):  # リスト・数
+    num = 0
+    for i in product([0, 1], repeat=l):
+        for j in range(l):
+            if i[j] == 1:  # 1 <-> bitが立っている時
+                # O(1)
+                pass
