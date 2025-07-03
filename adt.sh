9d5bd98a9bdb0eb28e6693dcb81afadb49cbe0b1
mitchi <pg.saku0719@gmail.com>
2025-07-03 20:20:30 +0900

    adt.sh

---
+++ adt.sh
@@ -0,0 +1,16 @@
+if [ -z "$1" ]; then
+    echo "Contest number is empty"
+else
+    if [ -d "./ADT/$1" ]; then
+        echo "./ADT/$1 already exists"
+    else
+        mkdir ./ADT/$1
+    fi
+
+    for p in A B C D E F G H; do
+        if [ ! -f "./ADT/$1/$p.py" ]; then
+            cp ./template/ADT.py "./ADT/$1/$p.py"
+        fi
+        code "./ADT/$1/$p.py"
+    done
+fi
