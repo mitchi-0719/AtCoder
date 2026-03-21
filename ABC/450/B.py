6cff3de5ece046e51898790f15f9ab13678683b9
mitchi <pg.saku0719@gmail.com>
2026-03-22 00:15:11 +0900

    abc450

---
+++ ABC/450/B.py
@@ -0,0 +1,39 @@
+# fmt: off
+from collections import *
+from itertools import *
+import bisect, copy, heapq, math, numpy, string, queue
+from sortedcontainers import *
+from functools import lru_cache
+import sys
+
+def I(): return int(sys.stdin.readline().rstrip()) # 数値
+def LI(): return list(map(int, sys.stdin.readline().rstrip().split())) # 数値リスト
+def S(): return sys.stdin.readline().rstrip() # 文字列
+def LS(): return list(sys.stdin.readline().rstrip().split()) # 文字列リスト
+def yes_no(b): return "Yes" if b else "No"
+def print_nobreak(t, end=""): print(t, end=end)
+
+sys.setrecursionlimit(10**8)
+mod = 998244353
+inf = float("inf")
+yes = "Yes"
+no = "No"
+
+dir8 = [(-1,-1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+dir4 = [(0, -1), (1, 0),(0, 1), (-1, 0)]
+
+# fmt: on
+
+n = I()
+cost = []
+
+for _ in range(n - 1):
+    cost.append([0] + LI())
+
+for C in combinations([i for i in range(n)], 3):
+    a, b, c = C
+    if cost[a][c - a] > cost[a][b - a] + cost[b][c - b]:
+        print(yes)
+        exit()
+
+print(no)
---
+++ ABC/450/C.py
@@ -0,0 +1,123 @@
+# fmt: off
+from collections import *
+from itertools import *
+import bisect, copy, heapq, math, numpy, string, queue
+from sortedcontainers import *
+from functools import lru_cache
+import sys
+
+def I(): return int(sys.stdin.readline().rstrip()) # 数値
+def LI(): return list(map(int, sys.stdin.readline().rstrip().split())) # 数値リスト
+def S(): return sys.stdin.readline().rstrip() # 文字列
+def LS(): return list(sys.stdin.readline().rstrip().split()) # 文字列リスト
+def yes_no(b): return "Yes" if b else "No"
+def print_nobreak(t, end=""): print(t, end=end)
+
+sys.setrecursionlimit(10**8)
+mod = 998244353
+inf = float("inf")
+yes = "Yes"
+no = "No"
+
+dir8 = [(-1,-1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+dir4 = [(0, -1), (1, 0),(0, 1), (-1, 0)]
+
+# fmt: on
+
+from collections import defaultdict
+
+
+class UnionFind:
+    # 各要素の親要素の番号を格納するリスト
+    # 要素が根（ルート）の場合は-(そのグループの要素数)を格納する
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    # 要素xが属するグループの根を返す
+    def root(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.root(self.parents[x])
+            return self.parents[x]
+
+    # 要素xが属するグループと要素yが属するグループとを併合する
+    def union(self, x, y):
+        x = self.root(x)
+        y = self.root(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    # 要素xが属するグループのサイズ（要素数）を返す
+    def size(self, x):
+        return -self.parents[self.root(x)]
+
+    # 要素x, yが同じグループに属するかどうかを返す
+    def same(self, x, y):
+        return self.root(x) == self.root(y)
+
+    # 要素xが属するグループに属する要素をリストで返す
+    def members(self, x):
+        root = self.root(x)
+        return [i for i in range(self.n) if self.root(i) == root]
+
+    # すべての根の要素をリストで返す
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    # グループの数を返す
+    def group_count(self):
+        return len(self.roots())
+
+    # {ルート要素: [そのグループに含まれる要素のリスト], ...}のdefaultdictを返す
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.root(member)].append(member)
+        return group_members
+
+
+def id(i, j):
+    return i * w + j
+
+
+h, w = LI()
+s = [list(S()) for _ in range(h)]
+wi = set()
+uf = UnionFind(h * w)
+
+for i in range(h):
+    for j in range(w):
+        if s[i][j] == ".":
+            for dx, dy in dir4:
+                if 0 <= i + dy < h and 0 <= j + dx < w and s[i + dy][j + dx] == ".":
+                    uf.union(id(i, j), id(i + dy, j + dx))
+
+for i in range(h):
+    for j in range(w):
+        if s[i][j] == ".":
+            wi.add(uf.root(id(i, j)))
+
+for i in range(h):
+    if uf.root(id(i, 0)) in wi:
+        wi.discard(uf.root(id(i, 0)))
+
+    if uf.root(id(i, w - 1)) in wi:
+        wi.discard(uf.root(id(i, w - 1)))
+
+for j in range(w):
+    if uf.root(id(0, j)) in wi:
+        wi.discard(uf.root(id(0, j)))
+
+    if uf.root(id(h - 1, j)) in wi:
+        wi.discard(uf.root(id(h - 1, j)))
+
+print(len(wi))
