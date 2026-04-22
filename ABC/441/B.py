5aef617afdee15b55741d496364ff938d34d0bef
mitchi <pg.saku0719@gmail.com>
2026-04-22 20:24:24 +0900

    abc441

---
+++ ABC/441/B.py
@@ -0,0 +1,47 @@
+# fmt: off
+from collections import *
+from itertools import *
+import bisect, copy, heapq, math, numpy, string, queue
+from sortedcontainers import *
+from functools import lru_cache
+import sys
+
+def I(): return int(sys.stdin.readline().rstrip()) # 数値
+def LI(): return list(map(int, sys.stdin.readline().rstrip().split())) # 数値リスト
+def S(): return sys.stdin.readline().rstrip() # 文字列
+def LS(): return list(sys.stdin.readline().rstrip().split()) # 文字列リスト
+def yes_no(b): return "Yes" if b else "No"
+def print_nobreak(t, end=""): print(t, end=end)
+
+sys.setrecursionlimit(10**8)
+mod = 998244353
+inf = float("inf")
+yes = "Yes"
+no = "No"
+
+dir8 = [(-1,-1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+dir4 = [(0, -1), (1, 0),(0, 1), (-1, 0)]
+
+# fmt: on
+
+n, m = LI()
+s = S()
+t = S()
+
+s_set = set(list(s))
+t_set = set(list(t))
+
+q = I()
+for _ in range(q):
+    w = S()
+    w_set = set(list(w))
+
+    is_s = w_set.issubset(s_set)
+    is_t = w_set.issubset(t_set)
+
+    if is_s and is_t:
+        print("Unknown")
+    elif is_s:
+        print("Takahashi")
+    elif is_t:
+        print("Aoki")
