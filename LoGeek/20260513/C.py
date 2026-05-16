fbcd3b01becbdde22754d7321835547246b61bbb
mitchi <pg.saku0719@gmail.com>
2026-05-16 14:58:58 +0900

    logeek20260513

---
+++ LoGeek/20260513/C.py
@@ -0,0 +1,38 @@
+# fmt: off
+from collections import *
+from itertools import *
+import bisect, copy, heapq, math, numpy, string, queue
+from sortedcontainers import *
+from functools import lru_cache
+import sys
+from pprint import pprint
+
+sys.setrecursionlimit(10**8)
+yes = "Yes"
+no = "No"
+mod = 998244353
+inf = float("inf")
+
+def I(): return int(sys.stdin.readline().rstrip()) # 数値
+def LI(): return list(map(int, sys.stdin.readline().rstrip().split())) # 数値リスト
+def S(): return sys.stdin.readline().rstrip() # 文字列
+def LS(): return list(sys.stdin.readline().rstrip().split()) # 文字列リスト
+def yes_no(b): return yes if b else no
+def sigma(n): (n * (n + 1)) // 2
+
+
+dir8 = [(-1,-1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+dir4 = [(0, -1), (1, 0),(0, 1), (-1, 0)]
+# fmt: on
+
+n = I()
+p = [0, 0] + LI()
+
+cur = n
+ans = 0
+
+while cur != 1:
+    cur = p[cur]
+    ans += 1
+
+print(ans)
---
+++ LoGeek/20260513/F.py
@@ -0,0 +1,47 @@
+# fmt: off
+from collections import *
+from itertools import *
+import bisect, copy, heapq, math, numpy, string, queue
+from sortedcontainers import *
+from functools import lru_cache
+import sys
+from pprint import pprint
+
+sys.setrecursionlimit(10**8)
+yes = "Yes"
+no = "No"
+mod = 998244353
+inf = float("inf")
+
+def I(): return int(sys.stdin.readline().rstrip()) # 数値
+def LI(): return list(map(int, sys.stdin.readline().rstrip().split())) # 数値リスト
+def S(): return sys.stdin.readline().rstrip() # 文字列
+def LS(): return list(sys.stdin.readline().rstrip().split()) # 文字列リスト
+def yes_no(b): return yes if b else no
+def sigma(n): return (n * (n + 1)) // 2
+
+
+dir8 = [(-1,-1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+dir4 = [(0, -1), (1, 0),(0, 1), (-1, 0)]
+# fmt: on
+
+n = I()
+ans = 0
+i = n
+j = 0
+while 1:
+    if n <= 0:
+        break
+
+    i = n % 10
+    if i == 0:
+        i = 10
+
+    if j != 0:
+        ans += sigma(i * 10**j - 1)
+
+    n //= 10
+    j += 1
+    print(sigma(i * 10**j - 1))
+
+print(ans % mod)
